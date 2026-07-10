2cd7e9dd100ba39dc664e5d6df840fb8d91b439c
ashmaddenwebmelbourne <ashleymadden55@gmail.com>
2026-07-10 15:42:57 +1000

    Refactored the is_valid_arguments function, wrote the execute_argument function, refactored main function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::path::Path;
 
 const VALID_STARTING_ARGUMENTS: [&str; 6] = [
     "add",
@@ -11,78 +12,69 @@ const VALID_STARTING_ARGUMENTS: [&str; 6] = [
 
 const VALID_LIST_ARGUMENTS: [&str; 3] = ["done", "todo", "in-progress"];
 
-// just checks that the argument format supplied is valid. doesn't validate further than that
-fn is_valid_arguments(arguments: &Vec<String>) -> bool {
-    // must have at least one argument
+// Checks that first command provided is valid
+fn is_valid_arguments(arguments: &Vec<String>) -> (bool, &str) {
     if arguments.len() == 0 {
-        false
+        (false, "Error: Must provide at least one argument")
     } else {
         let first_arg = arguments[0].to_lowercase();
-        // must start with a valid starting argument
-        if VALID_STARTING_ARGUMENTS.contains(&first_arg.as_str()) {
-            if first_arg != "list" {
-                true // first argument is add, update, delete, mark-in-progress or mark-done
-            } else {
-                if first_arg == "list" && arguments.len() == 1 {
-                    true // first argument is list, by itself
-                } else {
-                    // if starting argument is list and there are following arguments, it expects done, todo or in-progress to follow
-                    let list_arg = arguments[1].to_lowercase();
-                    if VALID_LIST_ARGUMENTS.contains(&list_arg.as_str()) {
-                        true // following argument is done, todo or in-progress 
-                    } else {
-                        false // not a valid list argument
-                    }
-                }
-            }
-        } else {
-            false // not a valid starting argument
+        match VALID_STARTING_ARGUMENTS.contains(&first_arg.as_str()) {
+            true => (true, ""), // command is add, update, delete, mark-in-progress, mark-done or list
+            false => (
+                false,
+                "Error: You must provide a valid argument to this program",
+            ),
         }
     }
 }
 
-// once validated, this function determines what command should be executed, calls the required function
-fn execute_argument() {}
+fn execute_argument(arguments: Vec<String>) {
+    let first_arg = &arguments[0].to_lowercase();
+    match first_arg.as_str() {
+        "add" => handle_add(arguments),
+        "update" => handle_update(arguments),
+        "delete" => handle_delete(arguments),
+        "mark-in-progress" => handle_mark_in_progress(arguments),
+        "mark-done" => handle_mark_done(arguments),
+        "handle_list" => handle_list(arguments),
+        _ => println!("Error: Invalid argument provided: {}", first_arg),
+    }
+}
 
 // these handle the functionality of the todo list, validating each command and argument(s) as needed
-fn handle_add() {}
+// CHECK that arguments are correct for <arg> command
+// UPDATE JSON file
+fn handle_add(arguments: Vec<String>) {}
 
-fn handle_update() {}
+fn handle_update(arguments: Vec<String>) {}
 
-fn handle_delete() {}
+fn handle_delete(arguments: Vec<String>) {}
 
-fn handle_mark_in_progress() {}
+fn handle_mark_in_progress(arguments: Vec<String>) {}
 
-fn handle_mark_done() {}
+fn handle_mark_done(arguments: Vec<String>) {}
 
-fn handle_list() {}
+// This should check for additional arguments after list command, such as done, todo , in-progress and pass them off accordingly.
+fn handle_list(arguments: Vec<String>) {}
 
-fn handle_list_done() {}
+fn handle_list_done(arguments: Vec<String>) {}
 
-fn handle_list_todo() {}
+fn handle_list_todo(arguments: Vec<String>) {}
 
-fn handle_list_in_progress() {}
+fn handle_list_in_progress(arguments: Vec<String>) {}
 
 fn main() {
-    // Grabs the arguments, but not the program name
-    let command_line_arguments: Vec<String> = env::args().skip(1).collect();
-
-    println!("{}", is_valid_arguments(&command_line_arguments));
-    println!("{:?}", command_line_arguments);
-
-    // JSON file should be stored in same directory as executable. file called todo.json
-
-    /*
-    Pseudocode:
-
-    PRESTEP 1: Check for todo.json file in same directory, return error if not found
-    STEP 1: PROMPT user for input
-    GET command line arguments
-    CHECK if command line arguments are valid
-    CALL execute_argument function
-        IF argument is <arg>, call handle_<arg> function
-            CHECK that arguments are correct for <arg> command
-                UPDATE JSON file
-                    GOTO STEP 1: PROMPT user for input
-    */
+    let todo_file = Path::new("todo.json");
+    if todo_file.exists() {
+        let command_line_arguments: Vec<String> = env::args().skip(1).collect();
+        let (is_valid_arguments, error_message) = is_valid_arguments(&command_line_arguments);
+        match is_valid_arguments {
+            true => execute_argument(command_line_arguments),
+            false => println!("{error_message}"),
+        }
+    } else {
+        println!(
+            "ERROR: You must have a todo.json file in the current directory to use. Create this file and then start the program again"
+        );
+    }
 }

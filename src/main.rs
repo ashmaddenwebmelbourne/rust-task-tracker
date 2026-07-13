3ca50c3c6c4a30b6af8681a351bf93f4e96a266d
ashmaddenwebmelbourne <ashleymadden55@gmail.com>
2026-07-13 18:49:40 +1000

    Udated validate_first_argument to use Result enum instead of (bool, &str) tuple, added check for empty todo file so that the basic fields can be added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 use std::path::Path;
 
 const VALID_FIRST_ARGUMENTS: [&str; 6] = [
@@ -15,32 +16,33 @@ const VALID_LIST_ARGUMENTS: [&str; 3] = ["done", "todo", "in-progress"];
 fn main() {
     let todo_file = Path::new("todo.json");
     if todo_file.exists() {
+        let todo_file_is_empty = fs::metadata(&todo_file).map(|metadata| metadata.len() == 0);
+        match todo_file_is_empty {
+            Ok(true) => create_blank_todo_list(),
+            _ => (),
+        }
         let command_line_arguments: Vec<String> = env::args().skip(1).collect();
-        let (is_valid_first_argument, error_message) =
-            validate_first_argument(&command_line_arguments);
-        match is_valid_first_argument {
-            true => handle_arguments(command_line_arguments), // Each command will validate itself later based on its own requirements, we just need a valid first command to proceed
-            false => println!("{error_message}"),
+        match validate_first_argument(&command_line_arguments) {
+            Ok(()) => handle_arguments(command_line_arguments), // Each command will validate itself later based on its own requirements, we just need a valid first command to proceed
+            Err(e) => println!("{}", e),
         }
     } else {
         println!("ERROR: Missing todo.json in current directory. Create it and try again");
     }
 }
 
-// Refactor to Result<(), &str> type for better success/error retrns
-fn validate_first_argument(arguments: &[String]) -> (bool, &str) {
+fn validate_first_argument(arguments: &[String]) -> Result<(), &str> {
     if arguments.is_empty() {
-        (false, "Error: Must provide at least one argument")
+        Err("Error: Must provide at least one argument")
     } else {
         let first_arg = arguments[0].to_lowercase();
         match VALID_FIRST_ARGUMENTS.contains(&first_arg.as_str()) {
-            true => (true, ""), // command is add, update, delete, mark-in-progress, mark-done or list
-            false => (false, "Error: Invalid first argument to this program"),
+            true => Ok(()), // command is add, update, delete, mark-in-progress, mark-done or list
+            false => Err("Error: Invalid first argument to this program"),
         }
     }
 }
 
-// Routes command to respective function handler
 fn handle_arguments(arguments: Vec<String>) {
     let first_arg = &arguments[0].to_lowercase();
     match first_arg.as_str() {
@@ -52,6 +54,30 @@ fn handle_arguments(arguments: Vec<String>) {
         "handle_list" => handle_list(arguments),
         _ => println!("Error: Invalid argument provided: {}", first_arg),
     }
+}
+
+// Handles the JSON file, ensuring it has the correct fields
+// When called this should set the following default fields in the JSON file, ready to be used as a todo list
+
+/*
+
+Note that taskName is an example placeholder value
+
+{
+  "tasks": {
+    "taskName": {
+      "id": 1,
+      "taskDescription": "Do something",
+      "status": "todo"
+    }
+  }
+}
+*/
+
+fn create_blank_todo_list() {
+    println!("Ready to add fields to JSON file");
+
+    // Add JSON fields here
 }
 
 // these handle the functionality of the todo list, validating each command and argument(s) as needed

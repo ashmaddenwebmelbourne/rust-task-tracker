12715d56bbf7530984f268e88a77e29dad2ea751
ashmaddenwebmelbourne <ashleymadden55@gmail.com>
2026-07-15 21:54:42 +1000

    Fixed every single clippy warning

--- src/main.rs
+++ src/main.rs
@@ -37,12 +37,13 @@ const _VALID_LIST_ARGUMENTS: [&str; 3] = ["done", "todo", "in-progress"];
 fn main() {
     let file = Path::new("todo.json");
     if file.exists() {
-        let is_empty_file = fs::metadata(&file).map(|data| data.len() == 0).unwrap();
+        let is_empty_file = fs::metadata(file).map(|data| data.len() == 0).unwrap();
         let command_line_arguments: Vec<String> = env::args().skip(1).collect();
 
-        match is_empty_file {
-            true => create_blank_todo_list(),
-            false => validate_todo_list_file(),
+        if is_empty_file {
+            create_blank_todo_list();
+        } else {
+            validate_todo_list_file();
         }
 
         match valid_first_argument(&command_line_arguments) {
@@ -61,9 +62,10 @@ fn valid_first_argument(arguments: &[String]) -> Result<(), &str> {
         Err("Error: Must provide at least one argument")
     } else {
         let first_arg = arguments[0].to_lowercase();
-        match VALID_FIRST_ARGUMENTS.contains(&first_arg.as_str()) {
-            true => Ok(()),
-            false => Err("Error: Invalid first argument to this program"),
+        if VALID_FIRST_ARGUMENTS.contains(&first_arg.as_str()) {
+            Ok(())
+        } else {
+            Err("Error: Invalid first argument to this program: {first_arg}")
         }
     }
 }
@@ -71,13 +73,13 @@ fn valid_first_argument(arguments: &[String]) -> Result<(), &str> {
 fn handle_arguments(arguments: Vec<String>) {
     let first_arg = &arguments[0].to_lowercase();
     match first_arg.as_str() {
-        "add" => handle_add(arguments),
+        "add" => handle_add(&arguments),
         "update" => handle_update(arguments),
         "delete" => handle_delete(arguments),
         "mark-in-progress" => handle_mark_in_progress(arguments),
         "mark-done" => handle_mark_done(arguments),
         "handle_list" => handle_list(arguments),
-        _ => println!("Error: Invalid argument provided: {}", first_arg),
+        _ => println!("Error: Invalid argument provided: {first_arg}"),
     }
 }
 
@@ -90,16 +92,16 @@ fn create_blank_todo_list() {
 // The file may exist and may not be empty, but it may have incorrect data in it. It needs to be in the right format, mentioned above
 fn validate_todo_list_file() {}
 
-fn handle_add(arguments: Vec<String>) {
+fn handle_add(arguments: &[String]) {
     let contents = fs::read_to_string("todo.json").unwrap();
     let mut todo_list: TodoList = serde_json::from_str(&contents).unwrap();
     let id = todo_list.tasks.iter().map(|t| t.id).max().unwrap_or(0) + 1;
     let task_name = arguments[1..].join(" ");
     if todo_list.tasks.iter().any(|t| t.name == task_name) {
-        println!("Error: A task with this name already exists: {}", task_name);
+        println!("Error: A task with this name already exists: {task_name}");
     } else {
         todo_list.tasks.push(Task {
-            id: id,
+            id,
             name: task_name,
             status: "todo".to_string(),
         });
